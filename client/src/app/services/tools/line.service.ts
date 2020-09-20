3043860980afbf221205cfe235750a689aca499e
tetreaultmarc <marc141195@gmail.com>
2020-09-19 20:39:46 -0400

    clicking twice creates a line without preview

--- client/src/app/services/tools/line.service.ts
+++ client/src/app/services/tools/line.service.ts
@@ -1,15 +1,33 @@
 import { Injectable } from '@angular/core';
 import { TOOL_NAMES } from '@app/../ressources/global-variables';
 import { Tool } from '@app/classes/tool';
+import { Vec2 } from '@app/classes/vec2';
 import { DrawingService } from '@app/services/drawing/drawing.service';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root',
 })
 export class LineService extends Tool {
-  name = TOOL_NAMES.LINE_TOOL_NAME;
+    name: string = TOOL_NAMES.LINE_TOOL_NAME;
+    startingMousePosition: Vec2;
+    endingCoordinates: Vec2;
+    isFirstClick: boolean = true;
 
-  constructor(drawingService: DrawingService) {
-    super(drawingService);
-  }
+    constructor(drawingService: DrawingService) {
+        super(drawingService);
+    }
+
+    onMouseDown(event: MouseEvent): void {
+        if (this.isFirstClick) {
+            this.startingMousePosition = this.getPositionFromMouse(event);
+            this.drawingService.previewCtx.beginPath();
+            this.drawingService.previewCtx.moveTo(this.startingMousePosition.x, this.startingMousePosition.y);
+            this.isFirstClick = false;
+        } else {
+            this.endingCoordinates = this.getPositionFromMouse(event);
+            this.drawingService.previewCtx.lineTo(this.endingCoordinates.x, this.endingCoordinates.y);
+            this.drawingService.previewCtx.stroke();
+            this.isFirstClick = true;
+        }
+    }
 }
