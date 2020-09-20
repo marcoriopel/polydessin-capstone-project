1df6197cb28a856de49330baafeffd877650600d
tetreaultmarc <marc141195@gmail.com>
2020-09-19 21:20:15 -0400

    added line preview on mouse move

--- client/src/app/services/tools/line.service.ts
+++ client/src/app/services/tools/line.service.ts
@@ -10,24 +10,56 @@ import { DrawingService } from '@app/services/drawing/drawing.service';
 export class LineService extends Tool {
     name: string = TOOL_NAMES.LINE_TOOL_NAME;
     startingMousePosition: Vec2;
-    endingCoordinates: Vec2;
-    isFirstClick: boolean = true;
+    endingMousePosition: Vec2;
+    isDrawing: boolean = false;
 
     constructor(drawingService: DrawingService) {
         super(drawingService);
     }
 
     onMouseDown(event: MouseEvent): void {
-        if (this.isFirstClick) {
+        this.mouseDown = true;
+    }
+
+    onMouseUp(event: MouseEvent): void {
+        this.mouseDown = false;
+        if (!this.isDrawing) {
+            // Record beginning of line position
             this.startingMousePosition = this.getPositionFromMouse(event);
-            this.drawingService.previewCtx.beginPath();
-            this.drawingService.previewCtx.moveTo(this.startingMousePosition.x, this.startingMousePosition.y);
-            this.isFirstClick = false;
+            this.isDrawing = true;
         } else {
-            this.endingCoordinates = this.getPositionFromMouse(event);
-            this.drawingService.previewCtx.lineTo(this.endingCoordinates.x, this.endingCoordinates.y);
+            // Record end of line position and draw line on base canvas
+            this.endingMousePosition = this.getPositionFromMouse(event);
+            this.drawLine(this.startingMousePosition, this.endingMousePosition, false);
+            this.isDrawing = false;
+        }
+    }
+
+    onMouseMove(event: MouseEvent): void {
+        if (!this.isDrawing) return;
+        // Clear the old line preview
+        this.drawingService.clearCanvas(this.drawingService.previewCtx);
+
+        // Get new coordinates for end of line
+        this.endingMousePosition = this.getPositionFromMouse(event);
+
+        // Draw the new line preview
+        this.drawLine(this.startingMousePosition, this.endingMousePosition, true);
+    }
+
+    drawLine(startingPoint: Vec2, endingPoint: Vec2, isPreview: boolean): void {
+        if (isPreview) {
+            // Using the preview canvas
+            this.drawingService.previewCtx.beginPath();
+            this.drawingService.previewCtx.moveTo(startingPoint.x, startingPoint.y);
+            this.drawingService.previewCtx.lineTo(endingPoint.x, endingPoint.y);
             this.drawingService.previewCtx.stroke();
-            this.isFirstClick = true;
+        } else {
+            // Using the base canvas
+            this.drawingService.baseCtx.beginPath();
+            this.drawingService.baseCtx.moveTo(startingPoint.x, startingPoint.y);
+            this.drawingService.baseCtx.lineTo(endingPoint.x, endingPoint.y);
+            this.drawingService.baseCtx.stroke();
         }
     }
 }
