ff9b2c0fef0c19646e3a780f82027b18576291f6
marco <marco.riopel@hotmail.com>
2020-09-23 15:30:17 -0400

    Trying to change color of svg for brush

--- client/src/app/services/tools/brush.service.ts
+++ client/src/app/services/tools/brush.service.ts
@@ -17,6 +17,7 @@ export class BrushService extends Tool {
     constructor(drawingService: DrawingService) {
         super(drawingService);
         this.clearPath();
+        this.loadImage();
     }
 
     handleCursor(): void {
@@ -61,22 +62,28 @@ export class BrushService extends Tool {
     }
     loadImage(): void {
         console.log('called');
+        this.image.style.fill = 'black';
         this.image = new Image();
-        this.image.src = 'https://i.imgur.com/huy6X9t.png';
+        this.image.style.fill = 'black';
+        // this.image.src = 'https://i.imgur.com/ph8BJoX.jpg';
+        this.image.src = '/assets/formal-invitation2.svg';
+        this.image.style.fill = 'black';
         console.log(this.image.complete);
     }
 
     private drawLine(ctx: CanvasRenderingContext2D, path: Vec2[]): void {
         ctx.lineWidth = this.width;
+        this.image.style.fill = 'black';
         ctx.lineJoin = ctx.lineCap = 'round';
         const pat = ctx.createPattern(this.image, 'repeat');
         if (pat instanceof CanvasPattern) {
+            this.image.style.fill = 'black';
             ctx.strokeStyle = pat;
-        } else if (pat == null) {
-            console.log('wtf');
+            this.image.style.fill = 'black';
         } else {
-            console.log(typeof pat);
+            console.log('Could not load pattern');
         }
+        this.image.style.fill = 'black';
         ctx.beginPath();
         for (const point of path) {
             ctx.lineTo(point.x, point.y);
@@ -87,61 +94,4 @@ export class BrushService extends Tool {
     private clearPath(): void {
         this.pathData = [];
     }
-    //     function midPointBtw(p1, p2) {
-    //   return {
-    //     x: p1.x + (p2.x - p1.x) / 2,
-    //     y: p1.y + (p2.y - p1.y) / 2
-    //   };
-    // }
-
-    // function getPattern() {
-    //   return ctx.createPattern(img, 'repeat');
-    // }
-
-    // var el = document.getElementById('c');
-    // var ctx = el.getContext('2d');
-
-    // ctx.lineWidth = 25;
-    // ctx.lineJoin = ctx.lineCap = 'round';
-
-    // var img = new Image;
-    // img.onload = function() {
-    //   ctx.strokeStyle = getPattern();
-    // };
-    // img.src = 'https://i.imgur.com/huy6X9t.png';
-
-    // var isDrawing, points = [ ];
-
-    // el.onmousedown = function(e) {
-    //   isDrawing = true;
-    //   points.push({ x: e.clientX, y: e.clientY });
-    // };
-
-    // el.onmousemove = function(e) {
-    //   if (!isDrawing) return;
-
-    //   points.push({ x: e.clientX, y: e.clientY });
-
-    //   ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-
-    //   var p1 = points[0];
-    //   var p2 = points[1];
-
-    //   ctx.beginPath();
-    //   ctx.moveTo(p1.x, p1.y);
-
-    //   for (var i = 1, len = points.length; i < len; i++) {
-    //     var midPoint = midPointBtw(p1, p2);
-    //     ctx.quadraticCurveTo(p1.x, p1.y, midPoint.x, midPoint.y);
-    //     p1 = points[i];
-    //     p2 = points[i+1];
-    //   }
-    //   ctx.lineTo(p1.x, p1.y);
-    //   ctx.stroke();
-    // };
-
-    // el.onmouseup = function() {
-    //   isDrawing = false;
-    //   points.length = 0;
-    // };
 }
