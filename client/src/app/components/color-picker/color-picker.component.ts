4f97b58eb0ab410734b59f79078b08cef7b162bc
tetreaultmarc <marc141195@gmail.com>
2020-09-25 10:06:52 -0400

    UI for color feature is done (could be improved)

--- client/src/app/components/color-picker/color-picker.component.ts
+++ client/src/app/components/color-picker/color-picker.component.ts
@@ -1,16 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-color-picker',
     templateUrl: './color-picker.component.html',
     styleUrls: ['./color-picker.component.scss'],
 })
-export class ColorPickerComponent implements OnInit {
+export class ColorPickerComponent {
     constructor() {}
+
+    primaryOpacity: number = 100;
+    secondaryOpacity: number = 100;
+    min: number = 0;
+    max: number = 100;
 
     changeColor(event: any) {
         console.log(event.target.value);
     }
 
-    ngOnInit(): void {}
+    swapColors(): void {}
+
+    decrementPrimaryOpacity(): void {
+        if (this.primaryOpacity > this.min) {
+            --this.primaryOpacity;
+        }
+    }
+
+    incrementPrimaryOpacity(): void {
+        if (this.primaryOpacity < this.max) {
+            ++this.primaryOpacity;
+        }
+    }
+
+    decrementSecondaryOpacity(): void {
+        if (this.secondaryOpacity > this.min) {
+            --this.secondaryOpacity;
+        }
+    }
+
+    incrementSecondaryOpacity(): void {
+        if (this.secondaryOpacity < this.max) {
+            ++this.secondaryOpacity;
+        }
+    }
 }
