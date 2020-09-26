592c45b5c84c180d3c9de08f4d37e0776bff2d81
Emilie31 <55295150+EmilieVaudrin@users.noreply.github.com>
2020-09-26 11:55:23 -0400

    modification of the picture link

--- client/src/app/ressources/texts/feature.ts
+++ client/src/app/ressources/texts/feature.ts
@@ -6,8 +6,8 @@ export interface feature {
 }
 
 export const GENERALS_FEATURE: feature[] = [
-    { name: 'Créer ou continuer un dessin', description: '...', picture: '@app/ressources/image/create_drawing.jpg' },
-    { name: 'Carrousel de dessin', description: '...', picture: '@app/ressources/image/picture_test.jpg' },
-    { name: 'Sauvegarde automatique et manuelle', description: '...', picture: '@app/ressources/image/picture_test.jpg' },
-    { name: 'Exportation', description: '...', picture: '@app/ressources/image/picture_test.jpg' },
+    { name: 'Créer ou continuer un dessin', description: '...', picture: './../../../assets/img/create_drawing.jpg' },
+    { name: 'Carrousel de dessin', description: '...', picture: './../../../assets/img/picture_test.jpg' },
+    { name: 'Sauvegarde automatique et manuelle', description: '...', picture: './../../../assets/img/picture_test.jpg' },
+    { name: 'Exportation', description: '...', picture: './../../../assets/img/picture_test.jpg' },
 ];
