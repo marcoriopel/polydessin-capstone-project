b7124aa1bb98f831f68fee0173f37199310f6398
tetreaultmarc <marc141195@gmail.com>
2020-09-27 14:02:57 -0400

    fixed the annoying console error

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -71,6 +71,6 @@ import { UserguideComponent } from './components/userguide/userguide.component';
     ],
     entryComponents: [UserguideComponent, MainPageComponent],
     providers: [],
-    bootstrap: [AppComponent, MainPageComponent],
+    bootstrap: [AppComponent],
 })
 export class AppModule {}
