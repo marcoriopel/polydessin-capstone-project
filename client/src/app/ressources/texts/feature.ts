645f77d4d4de997aece537180ccff08bf7d4f545
EmilieVaudrin <emilievaudrin@hotmail.fr>
2020-09-28 19:41:53 -0400

    Addind the video in userguide
    Former-commit-id: 472dab67d20afd6b4a1af8dbd5140ccf927afc91

--- client/src/app/ressources/texts/feature.ts
+++ client/src/app/ressources/texts/feature.ts
@@ -5,7 +5,7 @@ export interface Feature {
 }
 
 export const GENERALS_FEATURE: Feature[] = [
-    { name: 'Créer ou continuer un dessin', description: '...', picture: './../../../assets/img/create_drawing.jpg' },
+    { name: 'Créer ou continuer un dessin', description: '...', picture: './../../../assets/video/nouveau-dessin.gif' },
     { name: 'Carrousel de dessin', description: '...', picture: './../../../assets/img/picture_test.jpg' },
     { name: 'Sauvegarde automatique et manuelle', description: '...', picture: './../../../assets/img/picture_test.jpg' },
     { name: 'Exportation', description: '...', picture: './../../../assets/img/picture_test.jpg' },
