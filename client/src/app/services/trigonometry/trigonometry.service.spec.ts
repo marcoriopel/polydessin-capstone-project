7af6215c7ade6d33b284f9ce1b8a3b62aa019335
tetreaultmarc <marc141195@gmail.com>
2020-09-29 09:28:35 -0400

    switched fdescribe to describe

--- client/src/app/services/trigonometry/trigonometry.service.spec.ts
+++ client/src/app/services/trigonometry/trigonometry.service.spec.ts
@@ -3,7 +3,7 @@ import { LineAngle, Quadrant } from '@app/ressources/global-variables/global-var
 import { LIMIT_ANGLES } from '@app/ressources/global-variables/limit-angles';
 import { TrigonometryService } from './trigonometry.service';
 
-fdescribe('TrigonometryService', () => {
+describe('TrigonometryService', () => {
     let service: TrigonometryService;
 
     beforeEach(() => {
