c6ef52fca7219bfbfb431f5efea1490770fd18e4
Samuel Ouvrard <samuel.ouvrard@desjardins.com>
2020-09-29 22:13:31 -0400

    fixed rectangle double print on base and preview ctx

--- client/karma.conf.js
+++ client/karma.conf.js
@@ -14,6 +14,9 @@ module.exports = function (config) {
             require('@angular-devkit/build-angular/plugins/karma'),
         ],
         client: {
+            jasmine: {
+                seed: '01883',
+            },
             clearContext: false, // leave Jasmine Spec Runner output visible in browser
         },
         coverageIstanbulReporter: {
