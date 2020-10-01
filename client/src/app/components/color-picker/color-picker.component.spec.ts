9ded7d0a617e37466c69ec54d4e7655906448b99
marco <marco.riopel@hotmail.com>
2020-10-01 19:23:24 -0400

    Fixed ngModel binding for tests
    Former-commit-id: 8bb0b5297de504e4a608b563d8eca92237759c65

--- client/src/app/components/color-picker/color-picker.component.spec.ts
+++ client/src/app/components/color-picker/color-picker.component.spec.ts
@@ -1,5 +1,6 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
 import { MAXIMUM_NUMBER_OF_COLORS, MAX_OPACITY } from '@app/ressources/global-variables/global-variables';
 import { ColorPickerComponent } from './color-picker.component';
 
@@ -10,6 +11,7 @@ describe('ColorPickerComponent', () => {
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
+            imports: [FormsModule],
             schemas: [CUSTOM_ELEMENTS_SCHEMA],
             declarations: [ColorPickerComponent],
         }).compileComponents();
