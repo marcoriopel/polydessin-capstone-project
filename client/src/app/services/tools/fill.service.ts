b58f89747e9942a8546fe8ca4538194d07f239e7
Samuel Ouvrard <samuel.ouvrard@desjardins.com>
2020-10-17 18:09:51 -0400

    basic implementation of contiguousFill method of fill service using recursion
    Former-commit-id: 7926d4af220b8153b5caeb4878f40f8285477643

--- client/src/app/services/tools/fill.service.ts
+++ client/src/app/services/tools/fill.service.ts
@@ -15,6 +15,7 @@ export class FillService extends Tool {
     minTolerance: number = 0;
     tolerance: number = 0;
     mouseDownCoord: Vec2;
+    initialPixelData: Uint8ClampedArray;
 
     constructor(public drawingService: DrawingService, public colorSelectionService: ColorSelectionService) {
         super(drawingService);
@@ -31,16 +32,86 @@ export class FillService extends Tool {
 
     onMouseDown(event: MouseEvent): void {
         this.mouseDownCoord = this.getPositionFromMouse(event);
+        this.initialPixelData = this.drawingService.getPixelData(this.mouseDownCoord);
         if (event.button === MouseButton.Left) {
-            this.contiguousFill();
+            this.drawingService.baseCtx.fillStyle = this.colorSelectionService.primaryColor;
+            this.contiguousFill(this.mouseDownCoord);
         } else if (event.button === MouseButton.Right) {
             this.fill();
         }
     }
 
-    contiguousFill(): void {
-        // TODO
+    // contiguousFill(): void {
+    //     // TODO
+    // }
+
+    contiguousFill(pixel: Vec2): void {
+        const initialPixelData = this.initialPixelData;
+        const curentPixelData = this.drawingService.getPixelData(pixel);
+        if (
+            initialPixelData[0] - (this.tolerance * 255) / 100 <= curentPixelData[0] &&
+            curentPixelData[0] <= initialPixelData[0] + (this.tolerance * 255) / 100 &&
+            initialPixelData[1] - (this.tolerance * 255) / 100 <= curentPixelData[1] &&
+            curentPixelData[1] <= initialPixelData[1] + (this.tolerance * 255) / 100 &&
+            initialPixelData[2] - (this.tolerance * 255) / 100 <= curentPixelData[2] &&
+            curentPixelData[2] <= initialPixelData[2] + (this.tolerance * 255) / 100 &&
+            initialPixelData[3] - (this.tolerance * 255) / 100 <= curentPixelData[3] &&
+            curentPixelData[3] <= initialPixelData[3] + (this.tolerance * 255) / 100
+        ) {
+            this.drawingService.baseCtx.fillRect(pixel.x, pixel.y, 1, 1);
+
+            if (pixel.y - 1 >= 0) {
+                this.contiguousFill({ x: pixel.x, y: pixel.y - 1 });
+            }
+            if (pixel.y + 1 < this.drawingService.canvas.height) {
+                this.contiguousFill({ x: pixel.x, y: pixel.y + 1 });
+            }
+            if (pixel.x + 1 < this.drawingService.canvas.width) {
+                this.contiguousFill({ x: pixel.x + 1, y: pixel.y });
+            }
+            if (pixel.x - 1 >= 0) {
+                this.contiguousFill({ x: pixel.x - 1, y: pixel.y });
+            }
+        }
     }
+
+    // contiguousFill(): void {
+    //     const pixelData = this.drawingService.getPixelData(this.mouseDownCoord);
+    //     const stack: Vec2[] = [this.mouseDownCoord];
+    //     const canvasData: ImageData = this.drawingService.getCanvasData();
+
+    //     while (stack.length) {
+    //         const currentPixel = (stack.pop as unknown) as Vec2;
+    //         const index = currentPixel.x + currentPixel.y * this.drawingService.canvas.width;
+    //         if (this.isPrimaryColor(canvasData, index)) {
+    //             continue;
+    //         } else if (
+    //             pixelData[0] - (this.tolerance * 255) / 100 <= canvasData.data[index + 0] &&
+    //             canvasData.data[index + 0] <= pixelData[0] + (this.tolerance * 255) / 100 &&
+    //             pixelData[1] - (this.tolerance * 255) / 100 <= canvasData.data[index + 1] &&
+    //             canvasData.data[index + 1] <= pixelData[1] + (this.tolerance * 255) / 100 &&
+    //             pixelData[2] - (this.tolerance * 255) / 100 <= canvasData.data[index + 2] &&
+    //             canvasData.data[index + 2] <= pixelData[2] + (this.tolerance * 255) / 100 &&
+    //             pixelData[3] - (this.tolerance * 255) / 100 <= canvasData.data[index + 3] &&
+    //             canvasData.data[index + 3] <= pixelData[3] + (this.tolerance * 255) / 100
+    //         ) {
+    //             this.drawingService.baseCtx.fillRect(currentPixel.x, currentPixel.y, 1, 1);
+
+    //             if (currentPixel.y - 1 >= 0) {
+    //                 stack.push({ x: currentPixel.x, y: currentPixel.y - 1 });
+    //             }
+    //             if (currentPixel.y + 1 < this.drawingService.canvas.height) {
+    //                 stack.push({ x: currentPixel.x, y: currentPixel.y + 1 });
+    //             }
+    //             if (currentPixel.x + 1 < this.drawingService.canvas.width) {
+    //                 stack.push({ x: currentPixel.x + 1, y: currentPixel.y });
+    //             }
+    //             if (currentPixel.x - 1 >= 0) {
+    //                 stack.push({ x: currentPixel.x - 1, y: currentPixel.y });
+    //             }
+    //         }
+    //     }
+    // }
 
     fill(): void {
         const pixelData = this.drawingService.getPixelData(this.mouseDownCoord);
@@ -76,5 +147,26 @@ export class FillService extends Tool {
         }
 
         this.drawingService.baseCtx.putImageData(canvasData, 0, 0);
+    }
+
+    isPrimaryColor(canvasData: ImageData, index: number): boolean {
+        const primaryColor = this.colorSelectionService.primaryColor.slice(5);
+
+        const subStrings = primaryColor.split(',');
+        const r: number = parseInt(subStrings[0], 10);
+        const g: number = parseInt(subStrings[1], 10);
+        const b: number = parseInt(subStrings[2], 10);
+        const a: number = parseFloat(subStrings[3]) * 255;
+
+        if (
+            canvasData.data[index] === r &&
+            canvasData.data[index + 1] === g &&
+            canvasData.data[index + 2] === b &&
+            canvasData.data[index + 3] === a
+        ) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
