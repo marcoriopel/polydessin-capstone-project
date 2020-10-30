28039d67becfa7c10d3d6bb9a6d3701f414ed361
marco <marco.riopel@hotmail.com>
2020-10-30 17:43:15 -0400

    Added server side validation for carousel image preview
    Former-commit-id: 7a7113a8096eec0252663930e3e5551e8391f3b6

--- server/app/services/database.service.ts
+++ server/app/services/database.service.ts
@@ -13,6 +13,7 @@ const DATABASE_COLLECTION = 'Drawings';
 export class DatabaseService {
     collection: Collection<MetaData>;
     client: MongoClient;
+    DIR: string = './images';
 
     private options: MongoClientOptions = {
         useNewUrlParser: true,
@@ -61,94 +62,22 @@ export class DatabaseService {
         });
     }
 
-    // loadJSon(): string {
-    //     return fs.readFileSync('drawing.json').toString();
-    // }
-
-    // getDrawingsJson(): Drawing[] {
-    //     const drawings: Drawing[] = [];
-    //     const jsonContent = this.loadJSon();
-    //     const jsonObj = JSON.parse(jsonContent);
-    //     jsonObj.forEach((element: Drawing) => {
-    //         const drawing: Drawing = { id: element.id, drawingPng: element.drawingPng };
-    //         drawings.push(drawing);
-    //     });
-    //     return drawings;
-    // }
-
-    // async getDrawingData(files: string[]): Promise<FormData[]> {
-    //     const formDataArray: FormData[] = [];
-    //     const filterQuery: FilterQuery<DBData> = { fileName: { $in: files } };
-    //     let formData: FormData = new FormData();
-    //     return this.collection
-    //         .find(filterQuery)
-    //         .toArray()
-    //         .then((dBData: DBData[]) => {
-    //             files.forEach((file: string) => {
-    //                 dBData.forEach((dataFromDB: DBData) => {
-    //                     if (file === dataFromDB.fileName) {
-    //                         const PATH = './images/' + file;
-
-    //                         fs.readFile(PATH, (err, data) => {
-    //                             if (err) throw err; // Fail if the file can't be read.
-    //                             formData = new FormData();
-    //                             const oldData = data;
-    //                             formData.append(ID_NAME, dataFromDB.id);
-    //                             formData.append(NAME, dataFromDB.name);
-    //                             dataFromDB.tags.forEach((tag: string) => {
-    //                                 formData.append(TAGS_NAME, tag);
-    //                             });
-    //                             const blob = new Blob([oldData], { type: 'image/png' });
-    //                             formData.append('image', blob);
-    //                             formDataArray.push(formData);
-    //                         });
-    //                     }
-    //                 });
-    //             });
-    //             return formDataArray;
-    //         })
-    //         .catch(() => {
-    //             throw new Error('Error trying to retrieve metadata');
-    //         });
-    // }
-
-    // async getDrawingsPng(files: string[]): Promise<string[]> {
-    //     const filterQuery: FilterQuery<DBData> = { fileName: { $in: files } };
-    //     const images: string[] = [];
-    //     return this.collection
-    //         .find(filterQuery)
-    //         .toArray()
-    //         .then((dBData: DBData[]) => {
-    //             files.forEach((file: string) => {
-    //                 dBData.forEach((dataFromDB: DBData) => {
-    //                     if (file === dataFromDB.fileName) {
-    //                         images.push(file);
-    //                     }
-    //                 });
-    //             });
-    //             return images;
-    //         })
-    //         .catch(() => {
-    //             throw new Error('Error trying to retrieve metadata');
-    //         });
-    // }
-
     async getDBData(): Promise<DBData[]> {
         return this.collection
             .find()
             .toArray()
             .then((dBData: DBData[]) => {
-                return dBData;
+                const dBDataverified: DBData[] = [];
+                const files: string[] = fs.readdirSync(this.DIR);
+                for (const data of dBData) {
+                    if (files.includes(data.fileName)) {
+                        dBDataverified.push(data);
+                    }
+                }
+                return dBDataverified;
             })
             .catch(() => {
                 throw new Error('Error trying to retrieve metadata');
             });
     }
-    // getDrawingsIds(drawings: Drawing[]): string[] {
-    //     const ids: string[] = [];
-    //     drawings.forEach((element: Drawing) => {
-    //         ids.push(element.id);
-    //     });
-    //     return ids;
-    // }
 }
