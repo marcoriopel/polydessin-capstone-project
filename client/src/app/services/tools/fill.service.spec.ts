12aa8688417ad38802a34b950a5dafb3de95f1a1
Samuel Ouvrard <samuel.ouvrard@desjardins.com>
2020-11-01 20:19:42 -0500

    added one test to fill service spec to reach full branch coverage
    Former-commit-id: d4704b7c778950fee9d47abba797d493d2054733

--- client/src/app/services/tools/fill.service.spec.ts
+++ client/src/app/services/tools/fill.service.spec.ts
@@ -142,7 +142,51 @@ describe('FillService', () => {
         baseCtx.strokeRect(0, 0, 5, 5);
 
         // set the position of mouseDown event
-        service.mouseDownCoord = { x: 1, y: 1 };
+        service.mouseDownCoord = { x: 2, y: 2 };
+
+        // set up drawingServiceSpy with mock retrun values for test
+        drawingServiceSpy.baseCtx = baseCtx;
+        drawingServiceSpy.canvas = canvas;
+        drawingServiceSpy.getPixelData.and.returnValue(baseCtx.getImageData(service.mouseDownCoord.x, service.mouseDownCoord.y, 1, 1).data);
+        drawingServiceSpy.getCanvasData.and.returnValue(baseCtx.getImageData(0, 0, canvas.width, canvas.height));
+
+        // set rgba primary color to blue
+        const rgba: Rgba = {
+            RED: 0,
+            GREEN: 0,
+            BLUE: 255,
+            ALPHA: 255,
+        };
+        colorSelectionServiceSpy.getRgbaPrimaryColor.and.returnValue(rgba);
+
+        // start the test
+        service.contiguousFill();
+
+        const currentImageData: ImageData = baseCtx.getImageData(0, 0, canvas.width, canvas.height);
+        const expectedImageData: ImageData = baseCtxCompare.getImageData(0, 0, canvasCompare.width, canvasCompare.height);
+
+        expect(currentImageData).toEqual(expectedImageData);
+    });
+
+    it('contiguousFill should only fill inside canvas', () => {
+        // create a dummy canvas with a filled rectangle
+        const canvasCompare: HTMLCanvasElement = document.createElement('canvas');
+        canvasCompare.height = 10;
+        canvasCompare.width = 10;
+        const baseCtxCompare = canvasCompare.getContext('2d') as CanvasRenderingContext2D;
+        baseCtxCompare.fillStyle = 'blue';
+        baseCtxCompare.fillRect(0, 0, canvasCompare.width, canvasCompare.height);
+
+        // create a canvas with a rectangle that is not yet filled
+        const canvas: HTMLCanvasElement = document.createElement('canvas');
+        canvas.height = 10;
+        canvas.width = 10;
+        const baseCtx = canvas.getContext('2d') as CanvasRenderingContext2D;
+        baseCtx.fillStyle = 'white';
+        baseCtx.fillRect(-10, -10, 20, 20);
+
+        // set the position of mouseDown event
+        service.mouseDownCoord = { x: 0, y: 0 };
 
         // set up drawingServiceSpy with mock retrun values for test
         drawingServiceSpy.baseCtx = baseCtx;
