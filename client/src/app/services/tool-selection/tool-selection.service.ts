7c5cec4dc61bd9bbbdf2424ad32d4e4b2d85033e
EmilieVaudrin <emilievaudrin@hotmail.fr>
2020-11-05 17:37:39 -0500

    Fix shortcut pipette (i)
    Former-commit-id: ce4a9231a5a8c587a69d3ff7964ed558e103fed6

--- client/src/app/services/tool-selection/tool-selection.service.ts
+++ client/src/app/services/tool-selection/tool-selection.service.ts
@@ -87,7 +87,7 @@ export class ToolSelectionService {
     selectItem(toolName: string): void {
         switch (toolName) {
             case this.sidebarElements.NEW_DRAWING_NAME:
-                this.dialog.open(NewDrawingService);
+                this.newDrawingService.openWarningModal();
                 break;
             case this.sidebarElements.CAROUSEL_NAME:
                 this.dialog.open(CarouselComponent);
