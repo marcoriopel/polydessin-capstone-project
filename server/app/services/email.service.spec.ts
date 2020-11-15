5ec825f66f225b192bf9116ac7110063ee741009
Lilou Ba <lilouba@MacBook-Pro-de-Lilou.local>
2020-11-15 16:50:53 -0500

    Add export component before testing pipeline

---
+++ server/app/services/email.service.spec.ts
@@ -0,0 +1,16 @@
+// import { expect } from 'chai';
+
+// import { EmailService } from './email.service';
+
+// // describe('EmailService', () => {
+// //     let service: EmailService;
+
+// //     beforeEach(() => {
+// //         TestBed.configureTestingModule({});
+// //         service = TestBed.inject(EmailService);
+// //     });
+
+// //     it('should be created', () => {
+// //         expect(service).toBeTruthy();
+// //     });
+// // });
