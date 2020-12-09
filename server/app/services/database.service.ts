5302f8754a72225a2567f1c49fa59e32435103d6
marco <marco.riopel@hotmail.com>
2020-12-09 17:22:25 -0500

    Add environnment variables to project

--- server/app/services/database.service.ts
+++ server/app/services/database.service.ts
@@ -1,11 +1,18 @@
-import { DATABASE_COLLECTION, DATABASE_NAME, DATABASE_URL, MAX_NAME_LENGTH, MAX_NUMBER_TAG, MAX_TAG_LENGTH } from '@app/ressources/global-variables';
+import {
+    DATABASE_COLLECTION,
+    DATABASE_NAME,
+    DATABASE_URL_BEGINNING,
+    DATABASE_URL_END,
+    MAX_NAME_LENGTH,
+    MAX_NUMBER_TAG,
+    MAX_TAG_LENGTH,
+} from '@app/ressources/global-variables';
 import { DBData, MetaData } from '@common/communication/drawing-data';
 import * as fs from 'fs';
 import { injectable } from 'inversify';
 import { Collection, MongoClient, MongoClientOptions } from 'mongodb';
 import * as multer from 'multer';
 import 'reflect-metadata';
-
 @injectable()
 export class DatabaseService {
     collection: Collection<MetaData>;
@@ -13,7 +20,7 @@ export class DatabaseService {
     DIR: string = './images';
     IMAGE_PATH: string = '../../images/';
     multerObject: multer.Multer;
-    mongoURL: string = DATABASE_URL;
+    mongoURL: string = DATABASE_URL_BEGINNING + process.env.MONGO_USERNAME + ':' + process.env.MONGO_PASSWORD + DATABASE_URL_END;
     private options: MongoClientOptions = {
         useNewUrlParser: true,
         useUnifiedTopology: true,
